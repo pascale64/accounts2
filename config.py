315eb2c498f9205fccdc720034e7781db7b9ada7
pascale64 <38839334+pascale64@users.noreply.github.com>
2019-12-05 16:16:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+TOP_LEVEL_DIR = os.path.abspath(os.curdir)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://accounting:topdog@localhost/accounts'
+        'mysql+pymysql://accounting:topdog@localhost/account'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADS_DEFAULT_DEST = basedir + '/static/img/'
+    UPLOADS_DEFAULT_URL = 'http://192.168.0.11/static/img/'
+    UPLOADED_IMAGES_DEST = basedir + '/static/img/'
+    UPLOADED_IMAGES_URL = 'http://192.168.0.11/static/img/'
+    ITEMS_PER_PAGE = 25
